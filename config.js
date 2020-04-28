9018b0ca530360138e4e9a60e442cd18ad9dfdee
wkibbler <william.kibbler@googlemail.com>
2020-04-28 20:38:02 +0100

    pool

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ var refreshDelay = 30000;
 
 // pools stats by MainCoins
 var networkStat = [
-	["eu.blakepool.com", "http://eu.blakepool.com:8117"],
+	["blakepool.com", "http://eu.blakepool.com:8117"],
 	["cash2pool.us", "http://cash2pool.us:8117"],
 	["cash2.org", "https://pool1.cash2.org:8119"],
 	["solobox.com", "https://soloblox.com:8119"],
-	["cash.de", "https://cash2.de:8119"]
+	["cash2.de", "https://cash2.de:8119"]
 ];
 
 var networkStat2 = [
